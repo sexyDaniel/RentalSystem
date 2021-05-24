f91cad0fa0987f50787f382e083c0b1e013bcf70
Danil <danil.lazarev.90@mail.ru>
2021-05-24 16:01:30 +0300

    Relocate models for DB in DbModels

--- PapsCourse/Shared/DbModels/Area.cs
+++ PapsCourse/Shared/DbModels/Area.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PapsCourse.Shared.Models
+namespace PapsCourse.Shared.DbModels
 {
     public class Area
     {
--- PapsCourse/Shared/DbModels/User.cs
+++ PapsCourse/Shared/DbModels/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PapsCourse.Shared.Models
+namespace PapsCourse.Shared.DbModels
 {
     public class User
     {
