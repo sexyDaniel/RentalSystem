5219736c05e3ce166220c9d836d5117c126bff84
frgt10cs <ivan_gavrilov_2016@bk.ru>
2021-05-29 10:54:35 +0300

    added colors to statement table + user table statement

---
+++ PapsCourse/Shared/Models/Area/TableRentStatement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PapsCourse.Shared.Models.Area
+{
+    public class TableRentStatement
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public bool? IsSuccessful { get; set; }
+        public string Store { get; set; }
+        public int AreaId { get; set; }
+    }
+}
---
+++ PapsCourse/Shared/Models/Area/TableServiceStatement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PapsCourse.Shared.Models.Area
+{
+    public class TableServiceStatement
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public bool? IsSuccessful { get; set; }
+        public string Store { get; set; }
+        public int AreaId { get; set; }
+        public string Category { get; set; }
+    }
+}
