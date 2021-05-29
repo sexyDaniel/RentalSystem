38444018dff378917877946de851e8ea45b64586
frgt10cs <ivan_gavrilov_2016@bk.ru>
2021-05-29 16:11:50 +0300

    added store registration page

---
+++ PapsCourse/Shared/Models/Area/UserArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PapsCourse.Shared.Models.Area
+{
+    public class UserArea
+    {
+        public int Id { get; set; }
+        public string Store { get; set; }
+        public DateTime RentBefore { get; set; }
+    }
+}
